0db673b81c8b30937d79917d30902fa74677d17a
Olvenn <olga-vencel@yandex.ru>
2022-06-05 17:47:25 +0300

    list of products

--- project/src/components/catalog-list/catalog-list.tsx
+++ project/src/components/catalog-list/catalog-list.tsx
@@ -1,8 +1,14 @@
 import { useAppSelector } from '../../hooks/';
 import { getGuitars } from '../../store/reducers/selectors';
 import { CatalogItem } from '../catalog-item/catalog-item';
+import { fetchGuitarsAction } from '../../store/api-actions';
+import { store } from '../../store';
+import { getCurrentPage } from '../../store/reducers/selectors';
+import { ITEMS_PER_PAGE } from '../../const';
 
 export function CatalogList(): JSX.Element {
+  const currentPage = useAppSelector(getCurrentPage);
+  store.dispatch(fetchGuitarsAction(`?_start=${currentPage}&_limit=${ITEMS_PER_PAGE}`));
   const guitars = useAppSelector(getGuitars);
 
   return (
