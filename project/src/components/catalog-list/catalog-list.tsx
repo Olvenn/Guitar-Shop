9216415d7336a7d8ee2c024d74705f02a535708c
Olvenn <olga-vencel@yandex.ru>
2022-06-05 19:54:02 +0300

    add useEffect

--- project/src/components/catalog-list/catalog-list.tsx
+++ project/src/components/catalog-list/catalog-list.tsx
@@ -1,14 +1,20 @@
-import { useAppSelector } from '../../hooks/';
+import { useAppSelector, useAppDispatch } from '../../hooks/';
 import { getGuitars } from '../../store/reducers/selectors';
 import { CatalogItem } from '../catalog-item/catalog-item';
 import { fetchGuitarsAction } from '../../store/api-actions';
 import { store } from '../../store';
 import { getCurrentPage } from '../../store/reducers/selectors';
 import { ITEMS_PER_PAGE } from '../../const';
+import { useEffect } from 'react';
 
 export function CatalogList(): JSX.Element {
   const currentPage = useAppSelector(getCurrentPage);
-  store.dispatch(fetchGuitarsAction(`?_start=${currentPage}&_limit=${ITEMS_PER_PAGE}`));
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    store.dispatch(fetchGuitarsAction(`?_start=${currentPage}&_limit=${ITEMS_PER_PAGE}`));
+  }, [currentPage, dispatch]);
+
   const guitars = useAppSelector(getGuitars);
 
   return (
