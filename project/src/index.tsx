ccdf3e21cada5cc6d0f9b3e2d5ecd7493ba767ea
Olvenn <olga-vencel@yandex.ru>
2022-06-12 17:37:14 +0300

    conflicts fix3

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -5,12 +5,6 @@ import { store } from './store';
 import { App } from './components/app/app';
 import HistoryRouter from '../src/components/history-router/history-router';
 import browserHistory from './browser-history';
-<<<<<<< HEAD
-=======
-import { fetchReviewsAction } from './store/api-actions';
-
-store.dispatch(fetchReviewsAction());
->>>>>>> c81edab11eeb3f4cbea93529e666b372787e3844
 
 ReactDOM.render(
   <React.StrictMode>
