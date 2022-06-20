c302d7c74b1483e367143dc6f23251c636725a33
Olvenn <olga-vencel@yandex.ru>
2022-06-20 20:03:59 +0300

    Trial validation of the form for sending comments

--- project/src/components/review-modal/review-modal.tsx
+++ project/src/components/review-modal/review-modal.tsx
@@ -1,3 +1,5 @@
+import { useAppSelector } from '../../hooks/';
+import { getGuitar } from '../../store/reducers/selectors';
 import { Modal } from '../../components/modal/modal';
 import { ReviewForm } from '../../components/review-form/review-form';
 
@@ -7,12 +9,14 @@ type Props = {
 }
 
 export function ReviewModal({ onClose, onReviewAdd }: Props): JSX.Element {
+  const guitar = useAppSelector(getGuitar);
+
   return (
-    <div style={{ position: 'relative', width: '550px', height: '610px', marginBottom: '50px' }}>
-      <div className="modal is-active modal--success modal-for-ui-kit">
+    <div style={{ position: 'absolute', width: '550px', height: '610px', marginBottom: '50px' }}>
+      <div className="modal is-active modal--review modal-for-ui-kit">
         <Modal onClose={onClose} >
           <h2 className="modal__header modal__header--review title title--medium">Оставить отзыв</h2>
-          <h3 className="modal__product-name title title--medium-20 title--uppercase">СURT Z30 Plus</h3>
+          <h3 className="modal__product-name title title--medium-20 title--uppercase">{guitar?.name}</h3>
           <ReviewForm onReviewAdd={onReviewAdd} />
         </Modal>
       </div>
--- project/src/components/success-review-modal/success-review-modal.tsx
+++ project/src/components/success-review-modal/success-review-modal.tsx
@@ -13,16 +13,20 @@ export function SuccessReviewModal({ onClose }: SuccessReviewModalProps): JSX.El
   };
 
   return (
-    <Modal onClose={onClose} >
-      <svg className="modal__icon" width="26" height="20" aria-hidden="true">
-        <use xlinkHref="#icon-success" />
-      </svg>
-      <p className="modal__message">Спасибо за ваш отзыв!</p>
-      <div className="modal__button-container modal__button-container--review">
-        <button onClick={handleCloseClick} className="button button--small modal__button modal__button--review">
-          К покупкам!
-        </button>
+    <div style={{ position: 'absolute', width: '550px', height: '410px', marginBottom: '50px' }}>
+      <div className="is-active modal modal--success modal-for-ui-kit">
+        <Modal onClose={onClose} >
+          <svg className="modal__icon" width="26" height="20" aria-hidden="true">
+            <use xlinkHref="#icon-success" />
+          </svg>
+          <p className="modal__message">Спасибо за ваш отзыв!</p>
+          <div className="modal__button-container modal__button-container--review">
+            <button onClick={handleCloseClick} className="button button--small modal__button modal__button--review">
+              К покупкам!
+            </button>
+          </div>
+        </Modal>
       </div>
-    </Modal>
+    </div>
   );
 }
